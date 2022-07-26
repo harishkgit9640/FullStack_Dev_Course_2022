f49f4230b6254bd90a26b827e5220342db1608f2
Harish Kumar <harishkemail9640@gmail.com>
2022-07-26 20:10:49 +0530

    today I completed 2 chaper of the javaScript

---
+++ JavaScript/06_Operator&Epression.js
@@ -0,0 +1,63 @@
+// Operators and Expression in javaScript
+
+// console.log("Operators and Expression in javaScript");
+// Arithmatic Operator
+// let a = 10;
+// let b = 4;
+// console.log("a + b = ",  a + b);
+// console.log("a - b = ",  a - b);
+// console.log("a * b = ",  a * b);
+// console.log("a / b = ",  a / b);
+// console.log("a**b  = ",  a**b);
+// console.log("a % b = ",  a % b);
+// console.log("++a = ",  ++a);
+// console.log("a++ = ",  a++);
+// console.log("--a = ",  --a);
+// console.log("a-- = ",  a--);
+// console.log(a);
+
+// Assignment Operator
+// console.log("Assignment Operator in javaScript");
+
+// let a = 40;
+// let b = 6;
+
+// // a = 5;  // value change here
+// a += 5;     // x = x+y
+// a -= 5;     // x = x-y
+// a *= 5;     // x = x*y
+// a /= 5;     // x = x/y
+// a %= 5;     // x = x%y
+// a **= 5;     // x = x**y
+
+// console.log(a);
+
+
+console.log("Comparison Operator in javaScript");
+//  let a = 10;
+//  let b = 20;
+//  console.log(" is A Equall to B : " , a == b); // == check only values
+//  console.log(" is A not_Equall to B : " , a != b); // true
+//  console.log(" is A Equall to B : " , a === b); // === check the values + datatype
+//  console.log(" is A Equall to B : " , a !== b); // true
+//  console.log(" is A greater than B : " , a > b); // false
+//  console.log(" is A less than B : " , a < b); // true
+//  console.log(" is A greater than equal to B : " , a >= b); // false
+//  console.log(" is A less than equal to B : " , a <= b); // true
+
+
+console.log("Logical Operator in javaScript");
+// let a = 10;
+// let b = 20;
+
+// console.log(b>a && a==b);  // AND (&&) Operator, there is both conditions should be true.
+// console.log(b>a || a==b);  // OR (||) Operator, there is any one condition shoulb be true.
+// console.log(!false);  // NOT (!) Operator, there have any condition would be changed Into Opposite condition.
+
+
+console.log("Logical Operator in javaScript");
+let a = 10;
+let b = 20;
+
+
+ 
---
+++ JavaScript/07_conditional_Epression.js
@@ -0,0 +1,37 @@
+console.log('We are going to demostrate Conditional Epressions');
+
+// let age = prompt("What is your Age");
+// age = Number.parseInt(age);
+
+// if else comndition Demostration --->
+
+// if(age>0){
+//     alert("this the valid age");
+// }
+// else{
+//     alert("this the invalid age");
+// }
+
+
+
+// if else comndition Demostration --->
+// let age = prompt("What is your Age");
+// if(age<0 || age == ""){
+//     alert("Invalid value! Please Enter the valid value");
+// }
+// else if(age>=18){
+//     alert("you are able to drive a car");
+// }
+// else if(age<18 && age != 18){
+
+//     alert("you are not able to drive a car");
+// }
+
+// else{
+//     alert("You don't think about drive car chhote");
+// }
+
+
+// Terniry Operator example
+let age = 20;
+console.log("you can ", (age>18? "drive" : "not drive") );
