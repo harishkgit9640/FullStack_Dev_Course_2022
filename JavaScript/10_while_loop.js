fe5d170798cb2869e1232ba9301715e43cadaf08
Harish Kumar <harishkemail9640@gmail.com>
2022-07-30 18:46:42 +0530

    demostrating to while loop

---
+++ JavaScript/10_while_loop.js
@@ -0,0 +1,10 @@
+// Today' topic is While loop
+
+console.log("While Loop Demostrationd");
+let num = 10;
+// let n = prompt("Enter any number");
+let i = 1;
+while (i<num) {
+    console.log(i);
+    i++;
+}
