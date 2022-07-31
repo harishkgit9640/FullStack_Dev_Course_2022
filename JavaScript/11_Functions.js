c2af06f1f1a50a35c594dd0a5545dc4618ee2bde
Harish Kumar <harishkemail9640@gmail.com>
2022-07-31 08:43:23 +0530

    Function in javaScript

---
+++ JavaScript/11_Functions.js
@@ -0,0 +1,36 @@
+// #11 Functions in javascript
+
+// First Way of Use Function In javascript
+/*
+let a = 2;
+let b = 3;
+let c = 4;
+console.log("Average of a and b = ", (a + b)/2);
+console.log("Average of a and b = ", (b + c)/2);
+console.log("Average of a and b = ", (c + a)/2);
+*/
+
+/*
+// Second Way of Use Function In javascript
+let a = 2;
+let b = 3;
+let c = 4;
+
+function avrageOf(a, b) {
+    return Math.round((a+b)/2) ;
+}
+console.log("Average of a and b = ", avrageOf(a, b));
+console.log("Average of a and b = ", avrageOf(b, c));
+console.log("Average of a and b = ", avrageOf(c, a));
+*/
+
+// Third Way of Use Function In javascript
+const Hello = () =>{
+    console.log("Welcome to JavaScript course with codewithharry");
+}
+Hello();
+
+// Forth Way of Use Function In javascript
+const squar = (a, b) =>(a+b);
+console.log("Square of a and b =", squar(4,6));
+
