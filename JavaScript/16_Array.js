899a99e4581fcb6422182c616934d7be17e66adc
Harish Kumar <harishkemail9640@gmail.com>
2022-08-07 12:01:29 +0530

    some file added

---
+++ JavaScript/16_Array.js
@@ -0,0 +1,36 @@
+// Array in javaScript
+
+let cars = ['mercedes', 'BWM','Rolls Royce', 'Toyota'];
+
+console.log(cars[0]);
+console.log(cars[1]);
+console.log(cars[2]);
+
+
+let fruits = ['Apple','grapes','Orange','Banana'];
+
+console.log(fruits);
+
+
+// for (let value in fruits){
+//     console.log(fruits[value]);
+// }
+
+// for(let i = 0; i < fruits.length; i++){
+//     console.log(fruits[i])
+// }
+
+
+// Assignment - #1
+// let Std = {
+//     harish : 10,
+//     anmol : 20,
+//     pranay : 30,
+//     ramra : 40
+// }
+// for (let value in Std){
+//     console.log(value , "=" , Std[value]);
+// }
+
+
+
---
+++ JavaScript/17_Array_Methods.js
@@ -0,0 +1,18 @@
+// All about the JavaScript Mehods
+
+// #1 method .Pop()
+fruits.pop();   // removes from the end
+console.log(fruits);
+
+// #2 method .push()
+fruits.push('Pineapple'); // Add the value at the end
+console.log(fruits);
+
+// #3 method .shift()
+fruits.shift();     // removes from the first
+console.log(fruits);
+
+
+// #4 method .unshift()
+fruits.unshift("papaya");  // Add the value at the first
+console.log(fruits);
