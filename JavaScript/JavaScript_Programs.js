c9da8cfcfab70f7eac2093a57584d020287b96eb
Harish Kumar <harishstudio877@gmail.com>
2022-08-15 11:31:34 +0530

    I have completed 10 Questions of JavaScript program

--- JavaScript/JavaScript_Programs.js
+++ JavaScript/JavaScript_Programs.js
@@ -27,7 +27,7 @@
 // alert(`The sum of ${a} and ${b} is: = ${sum}`);
 
 //===================//========================//===================
-// Question No.03
+// Question No.03 - JavaScript program to print the squre of the given number.
 
 // const number1 = parseInt(prompt("Enter the value"));
 // const result1 = Math.sqrt(number1);
@@ -43,6 +43,104 @@
 // alert(`The square root of ${number3} is ${result3}`);
 
 //===================//========================//===================
-// Question No.04
+// Question No.04 - JavaScript program to calculate the  area of a Rectangle.
+
+// let base = parseInt(prompt("Enter the Base of a Triangle : "));
+// let height = parseInt(prompt("Enter the Base of a Height : "));
+// let Area = (base * height) / 2;
+// alert(`The Area of Triangle is: = ${Area}`);
 
 
+
+//===================//========================//===================
+//Question No.05 JavaScript program to swap two variables
+
+// let a = parseInt(prompt("Enter The value of a:"))
+// let b = parseInt(prompt("Enter The value of b:"))
+// let swap;
+
+// swap = a;
+// a = b;
+// b = swap;
+
+// alert(`Swaped value a is: ${a}`);
+// alert(`Swaped value b is: ${b}`);
+
+//===================//========================//===================
+//Question No.06 - JavaScript Program to Convert Kilometers to Miles
+// let kilometers = 5;
+
+// let miles = (0.621371 * kilometers);
+// console.log(miles);  // 3.106855 miles
+
+//===================//========================//===================
+//Question No.07 -Javascript Program to Convert Celsius to Fahrenheit
+
+// let celsius = parseInt(prompt("Enter The value Celsius:"))
+// let Fehrenheit = celsius * 1.8 + 32;
+
+// let Fehrenheit = parseInt(prompt("Enter The Fehrenheit value :"))
+// let Celsius = (Fehrenheit - 32) * 5 / 9
+// alert(Celsius);
+
+//===================//========================//===================
+//Question No.08 -JavaScript Program To Work With Constants
+
+/*
+// program to include constants
+
+const a = 5;
+console.log(a);
+
+// constants are block-scoped
+{
+    const a = 50;
+    console.log(a);
+}
+console.log(a);
+
+const arr = ['work', 'exercise', 'eat'];
+console.log(arr);
+
+// add elements to arr array
+arr[3] = 'hello';
+console.log(arr);
+
+// the following code gives error
+// changing the value of a throws an error
+// uncomment to verify
+// a = 8;
+// console.log(a);
+
+// throws an error
+// const x;
+*/
+
+//===================//========================//===================
+//Question No.09 - Javascript Program to Generate a Random Number
+
+// let arr = [];
+// let num;
+// num = Math.random() * (10 - 1) + 1;
+// let num1 = Math.floor(num);
+// arr.push(num1);
+
+// console.log(`The Array is After Storing Randam Number : ${arr}`);
+
+//===================//========================//===================
+//Question No.10 - Javascript Program to Check if a number is Positive, Negative, or Zero
+
+let number = parseInt(prompt("Enter The Number"));
+
+// let number = 10;
+if (number == 0) {
+    alert("Enterd number is ZERO ");
+}
+if (number > 0) {
+    alert("Enterd number is Passitive ");
+} else {
+    alert("Enterd number is Negetive ");
+}
+
+//===================//========================//===================
+//Question No.11 -
