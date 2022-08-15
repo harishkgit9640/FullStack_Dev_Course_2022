b8694cf80e0c099e255bae60d7e7740df62c56e4
Harish Kumar <harishstudio877@gmail.com>
2022-08-15 10:13:32 +0530

    JavaScript Practice

---
+++ JavaScript/JavaScript_Programs.js
@@ -0,0 +1,48 @@
+
+// Question no.1 - JavaScript Program to Print Hello World.
+// we have there are three ways to print hello world.
+
+// First ways- Console.log()
+// console.log("Hello World");
+
+// Second way- Alert()
+// alert("hello world");
+
+// thirth way- document.write()
+// document.write("hello world");
+
+//===================//========================//===================
+
+// Question No.02 - JavaScript program to Add To number
+
+// let a = 10;
+// let b = 40;
+// let sum = a + b;
+// console.log(`The sum of ${a} and ${b} is: = ${sum}`);
+
+// Add To number Entered by User
+// const a = parseInt(prompt("Enter The First Value"));
+// const b = parseInt(prompt("Enter The second Value"));
+// let sum = a + b;
+// alert(`The sum of ${a} and ${b} is: = ${sum}`);
+
+//===================//========================//===================
+// Question No.03
+
+// const number1 = parseInt(prompt("Enter the value"));
+// const result1 = Math.sqrt(number1);
+
+// const number2 = -8;
+// const result2 = Math.sqrt(number2);
+
+// const number3 = "49a";
+// const result3 = Math.sqrt(number3);
+
+// alert(`The square root of ${number1} is ${result1}`);
+// alert(`The square root of ${number2} is ${result2}`);
+// alert(`The square root of ${number3} is ${result3}`);
+
+//===================//========================//===================
+// Question No.04
+
+
