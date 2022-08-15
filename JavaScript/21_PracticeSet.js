0a739b826c44d928d21291e37d0c3c59272e4ed5
Harish Kumar <harishstudio877@gmail.com>
2022-08-15 08:29:04 +0530

    I have completed 21th videos of JavaScript

---
+++ JavaScript/21_PracticeSet.js
@@ -0,0 +1,46 @@
+// Practice Set - 05
+
+// Problem No - 01
+
+// let arr = [1, 3, 5, 5, 6, 7, 7, 8,];
+// let num = prompt("Enter the Number");
+// num = parseInt(num);
+// arr.push(num);
+// console.log(arr);
+
+// Problem No - 02
+// let arr = [1, 3, 5];
+// let num;
+// do {
+//     num = prompt("Enter the Number");
+//     num = parseInt(num);
+//     console.log(num);
+//     arr.push(num);
+// }
+// while (num != 0);
+// console.log(arr);
+
+// problem no. - 3
+
+// let arr = [20, 23, 4, 5, 67, 8, 40, 234, 40, 240];
+// let newArr = arr.filter((x) => {
+//     return x % 10 == 0;
+// });
+// console.log(newArr);
+
+
+// problem no. - 4
+// let arr = [2, 3, 4, 5];
+// let newArr = arr.reduce((x, y) => {
+//     return x * y;
+// });
+// console.log(newArr);
+
+// problem no. - 5
+// let arr = [2, 3, 4];
+// let num = 1;
+// let number = 5;
+// for (let i = 1; i <= number; i++) {
+//     num = num * i
+//     console.log(num);
+// }
