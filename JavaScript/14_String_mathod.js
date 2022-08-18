3de096339d853cff8289358104c05c97e5d270b2
Harish Kumar <harishstudio877@gmail.com>
2022-08-18 22:06:26 +0530

    javascript practice.

--- JavaScript/14_String_mathod.js
+++ JavaScript/14_String_mathod.js
@@ -10,3 +10,17 @@ let Name2 = "Raj";
 // // console.log(Name.replace("Ha","Ma"));
 // console.log(Name1.concat(" , I\'m one of the Student of Master Spoken Class"));
 // console.log(Name1.trim());
+
+
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+let arr = [];
+let num = parseInt(prompt("Enter The number:"));
+while (num > 0) {
+    let num = parseInt(prompt("Enter The number:"));
+    arr.push(num);
+    if (num == 0) {
+        break;
+    }
+}
+console.log(arr);
+// console.log(arr.reverse());
