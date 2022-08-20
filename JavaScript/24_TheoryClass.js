52320d3ae17a01cc01aa107c0904d70848c6b716
Harish Kumar <harishstudio877@gmail.com>
2022-08-20 10:42:54 +0530

    some file added

---
+++ JavaScript/24_TheoryClass.js
@@ -0,0 +1,1 @@
+// We Descussed about console functions
---
+++ JavaScript/25_JS_console.js
@@ -0,0 +1,15 @@
+// There are many type of console in javaScript which is following below; 
+
+// console.log(console); // console stores all the console's method
+
+console.warn("This is Warn method")
+console.info("This is info method")
+console.clear("This is clear method")
+console.assert("This is assert method")
+console.table("This is table method")
+console.timeEnd("This is timeEnd method")
+console.time("This is time method")
+console.dir("This is dir method")
+console.error("This is error method")
+console.profile("This is profile method")
+console.context("This is context method")
