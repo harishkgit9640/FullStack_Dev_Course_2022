8f7c84c0acc5928d62e2bae5664f1223b0066e09
Harish Kumar <harishstudio877@gmail.com>
2022-08-29 21:36:59 +0530

    assignment completed

--- JavaScript/Project.js
+++ JavaScript/Project.js
@@ -1,6 +1,31 @@
-let age = prompt("Enter Your Age:");
-if (age >= 18) {
-    alert("You Can Drive!");
-} else {
-    alert("You Can not Drive!");
+/*
+let carDrive = (age) => {
+    return age >= 18 ? true : false;
 }
+// let age = 34;
+let runAgain = true;
+while (runAgain) {
+    let age = prompt("Enter Your Age:");
+    if (age < 0) {
+        console.error("Invalid Age! Please Enter valid Age.")
+        break;
+    }
+    if (carDrive(age)) {
+        alert("You Can Drive!");
+    } else {
+        alert("You Can not Drive!");
+    }
+    runAgain = confirm("Do you Want to play Again!");
+
+}
+*/
+
+// let number = parseInt(prompt("Enter The number!"))
+
+// if (number > 4) {
+//     location.href = "https://www.google.com/";
+// }
+
+color = prompt("Enter Your Favorite color ");
+document.body.style.backgroundColor = color;
+
