ec503fc2204263bfcaf79360e921c208b430ed74
Harish Kumar <harishstudio877@gmail.com>
2022-09-03 09:42:05 +0530

    continue

--- Music_player/script.js
+++ Music_player/script.js
@@ -4,9 +4,10 @@ const wrapper = document.querySelector(".wrapper"),
     musicImg = wrapper.querySelector(".img_area img"),
     musicName = wrapper.querySelector(".song_details .name"),
     musicArtist = wrapper.querySelector(".song_details .artist"),
-    mainAudio = wrapper.querySelector("#main-audio");
+    mainAudio = wrapper.querySelector("#main-audio"),
+    playPauseBtn = wrapper.querySelector(".play_pause");
 
-let musicIndex = 6;
+let musicIndex = 2;
 
 window.addEventListener("load", () => {
     loadMusic(musicIndex);
@@ -18,3 +19,21 @@ function loadMusic(indexNumb) {
     musicImg.src = `imgs/${music_list[indexNumb - 1].img}.jpg`;
     mainAudio.src = `assets/${music_list[indexNumb - 1].src}.m4a`;
 }
+
+
+
+function playMusic() {
+    wrapper.classList.add("paused");
+    mainAudio.play();
+}
+
+function pauseMusic() {
+    wrapper.classList.remove("paused");
+    // playPauseBtn.querySelector("i").innerHTML = "play_arrow"
+    mainAudio.pause();
+}
+
+playPauseBtn.addEventListener('click', () => {
+    const isMusicPause = wrapper.classList.contains("paused");
+    isMusicPause ? pauseMusic() : playMusic();
+});
