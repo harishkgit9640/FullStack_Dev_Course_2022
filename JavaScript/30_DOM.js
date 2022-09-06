2f2c8a7dbb7db8092a4fc6eeec9faf6fe37ff112
Harish Kumar <harishstudio877@gmail.com>
2022-09-06 21:06:21 +0530

    I habe done my todays class

---
+++ JavaScript/30_DOM.js
@@ -0,0 +1,16 @@
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+// console.log(document.body.childNodes);
+
+// let arr = Array.from((document.body.childNodes));
+
+// matches closest and contains methods
+
+const Heading = document.getElementById('heading')
+const div = document.getElementById('div_box')
+const span = document.getElementById('sp1')
+// console.log(Heading);
+
+// console.log(div.matches('.box'));
+// console.log(span.closest('#sp1'));
+console.log(contains('.box'));
