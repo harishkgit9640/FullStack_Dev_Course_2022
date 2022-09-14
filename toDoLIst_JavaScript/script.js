6f670b6f83623e552c37940c603f02d2218bc989
Harish Kumar <harishstudio877@gmail.com>
2022-09-14 17:54:28 +0530

    some changes on todo list project

--- toDoLIst_JavaScript/script.js
+++ toDoLIst_JavaScript/script.js
@@ -2,20 +2,40 @@
 
 let inpNewTask = $('#inpNewTask');
 let ulTask = $('#ulTask');
-let addTask = $('#addTask');
-let sortTask = $('#sortTask');
-let resetTask = $('#resetTask');
-let clearTask = $('#clearTask');
+let addBtn = $('#addTask');
+let sortBtn = $('#sortTask');
+let resetBtn = $('#restTask');
+let delBtn = $('#clearTask');
 
-addTask.click(() => {
+
+function addItem() {
     let listItem = $('<li>', {
-        'class': 'list-group-item',
+        'class': 'list-group-item list-group-item-primary',
         text: inpNewTask.val()
-    })
+    });
+    listItem.click((event) => {
+        listItem.toggleClass('done');
+    });
     ulTask.append(listItem)
+    inpNewTask.val('');
+}
+
+function clearDone() {
     inpNewTask.val('')
-});
+}
 
-resetTask.click(() => {
+function sortTask() {
+    inpNewTask.val('sorted')
 
-});
+}
+
+function delTask() {
+    inpNewTask.val('deleted')
+}
+
+
+addBtn.click(addItem);
+resetBtn.click(clearDone);
+sortBtn.click(sortTask);
+delBtn.click(delTask);
+
