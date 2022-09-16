fef0879cb7b5d9c503a393ade2fec7365d83d965
Harish Kumar <harishstudio877@gmail.com>
2022-09-16 23:05:33 +0530

    I have completed

---
+++ JavaScript/Chapter_08.js
@@ -0,0 +1,103 @@
+
+/*
+  // class - 41
+  function Class_41() {
+    document.getElementById('span1').innerHTML = 'I am span1'
+    document.getElementById('span2').innerHTML = 'I am span2'
+    let span2 = document.getElementById('span2')
+    let innerHTML = document.getElementById('span2').innerHTML
+    let outerHTML = document.getElementById('span2').outerHTML
+    let textContent = document.getElementById('span2').textContent
+    console.dir(span2);
+    console.log(innerHTML);
+    console.log(outerHTML);
+    console.log(textContent);
+  }
+  */
+
+/*
+// class - 42
+function Class_42() {
+  let box = document.querySelector('#box')
+  // let mainBox = box.getAttribute('class');
+  // console.log(box.hasAttribute('class'));
+  // let mainBox = box.setAttribute('src', '/img/img_1');
+  // let mainBox = box.setAttribute('hidden', 'false');
+  // let mainBox = box.removeAttribute('hidden', 'false');
+  // let mainBox = box.attributes;
+  // console.log(box.dataset);
+  // console.log(box.dataset.player);
+ 
+  // console.log(box);
+  // console.log(mainBox);
+ 
+}
+*/
+
+/*
+  // class - 43
+  function Class_43() {
+    let box = document.querySelector('#box');
+    // box.innerHTML = '<h1>WELCOME TO JAVASCRIPT</h1>';
+ 
+    //using loop
+    // for (let i = 1; i < 5; i++) {
+    //   let p = document.createElement('p');
+    //   p.innerHTML = 'IAM PARA'
+    //   box.append(p);
+    // }
+ 
+    let p = document.createElement('p');
+    p.innerHTML = ' <h1>IAM PARA</h1>'
+    box.prepend(p);  // At the beginning of the element
+    // box.append(p);  // At the end of the element
+    // box.before(p);  // At the before of the element
+    // box.after(p);  // At the after of the element
+    // box.replaceWith(p);  // At the Replace from the element
+  }
+  */
+
+/*
+// class - 44
+function Class_44() {
+let box = document.querySelector('#box');
+// box.insertAdjacentHTML('beforebegin', 'JavaScript')
+// box.insertAdjacentHTML('beforeend', '<h1>beforeend</h1>')
+// box.insertAdjacentHTML('afterend', '<h1>afterend</h1>')
+// box.insertAdjacentHTML('afterbegin', '<h1>afterbegin</h1>')
+box.remove();
+}
+*/
+
+/*
+  // class - 45
+  function Class_45() {
+    let box = document.querySelector('#box')
+    // box.className = 'lightMode'
+    // box.classList.add('lightMode')
+    // box.classList.remove('darkMode')
+    // box.classList.toggle('lightMode')
+    // document.querySelector('span').innerHTML = '<h1>Light Mode Enabled</h1>';
+  }
+*/
+
+/*
+// class - 46
+function Class_46() {
+  let box = document.querySelector('#box');
+ 
+  // setTimeInterval Runs Infinite according to set TimeInterval  
+  // setInterval(() => {
+  //   box.classList.toggle('lightMode')
+  //   document.querySelector('span').innerHTML = '<h1>Light Mode Enabled</h1>';
+  // }, 2000);
+ 
+ 
+ 
+  // setTimeout Run onces
+  setTimeout(() => {
+    box.classList.toggle('lightMode')
+  }, 2000);
+ 
+}
+*/
---
+++ JavaScript/Function.js
@@ -0,0 +1,11 @@
+
+
+let sum = (a, b) => {
+    let sum = a + b;
+    console.log("The Sum of two number" + sum);
+    return add;
+}
+
+let multi = () => {
+    console.log("The Multi of two number");
+}
