51619cff6fa1c70874501ce767a369e9c26b5929
Harish Kumar <harishstudio877@gmail.com>
2022-09-23 23:24:52 +0530

    ok

--- JavaScript/Event.js
+++ JavaScript/Event.js
@@ -1,6 +1,12 @@
 
 // class - 47
 function Class_47() {
-
     console.log("This is the Function");
+    let d = new Date();
+    let m = d.getMonth();
+    let day = d.getDay();
+    let y = d.getFullYear();
+    console.log(m, day, y);
+
 }
+Class_47();
