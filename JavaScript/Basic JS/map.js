6c6d1e174257de8ca090100aa76b4a1b14142bcc
Harish Kumar <harishstudio877@gmail.com>
2022-09-25 21:38:06 +0530

    some changes on code

--- JavaScript/Basic JS/map.js
+++ JavaScript/Basic JS/map.js
@@ -1,7 +1,10 @@
 import { sData, teacherData } from "./stdData.js";
 // let arr = [1, 3, 4, 5, 6, 7, [10, 29, 49, 59]];
-console.log(teacherData[0].fname);
+
+const wrapper = document.querySelector('.wrapper');
 sData.map((val, index, arr) => {
-    console.log(`${val.fname} ${val.lname}`);
+    let li = document.createElement('li')
+    li.innerHTML = `${val.fname} ${val.lname}`;
+    wrapper.append(li);
     // console.log(index);
 });
