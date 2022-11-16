9ec733c0c4aeb3abb2666f8824b4619a0b067d20
Harish Kumar <harishstudio877@gmail.com>
2022-11-16 18:24:11 +0530

    server starter template

--- JavaScript/example.js
+++ JavaScript/example.js
@@ -1,1 +1,15 @@
-let fullName = "harish Kumar sahu ";
+
+
+// server creation
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+
+    console.log('hello world, I M form server');
+});
+
+// app.listen(3000, (req, res) => {
+//     console.log('server listening on port 3000');
+// });
