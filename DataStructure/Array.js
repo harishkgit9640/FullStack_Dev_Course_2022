432c58823184f25683cdf8c8f172ae54302300e7
Harish Kumar <harishstudio877@gmail.com>
2022-12-04 13:45:10 +0530

    one js dsa question solved

--- DataStructure/Array.js
+++ DataStructure/Array.js
@@ -13,3 +13,27 @@ let zeroSum = (arr) => {
 console.log(zeroSum(arr));
 */
 
+
+// find the even number of elements and then make double each number and print the length of the array.
+
+/* 
+
+let arr = [1, 2, 4, 5, 6, 8, 9, 10, 12, 13];
+let newArr = [];
+let counter = 0;
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+        counter++;
+        newArr.push(arr[i] * 2);
+    }
+}
+console.log(newArr);  //[ 4, 8, 12, 16, 20, 24 ]
+console.log(counter); // 6
+
+
+*/
+
+
+
+
+
