85781c9401ef26d1fb7e947b57ad049771bb35d1
Harish Kumar <harishstudio877@gmail.com>
2022-12-04 10:00:54 +0530

    some changes

--- DataStructure/Pattern.js
+++ DataStructure/Pattern.js
@@ -22,14 +22,21 @@ console.log(show);
 
 // ==============================|||============================
 
-// let show = "";
-// for (i = 5; i >= 1; i--) {
-//     for (j = 0; j < i; j++) {
-//         show += " * ";
-//     }
-//     show += "\n";
-// }
-// console.log(show);
+
+let row = 5;
+let show = "";
+for (i = 1; i <= row; i++) {
+
+    for (s = 0; s > row - i; s++) {
+        show = "";
+    }
+
+    for (j = 0; j < (2 * i - row); j++) {
+        show += " * ";
+    }
+    show += "\n";
+}
+console.log(show);
 
 // output of the above code
 
@@ -43,3 +50,15 @@ console.log(show);
 */
 
 // ==============================|||============================
+
+
+// let show = "";
+// for (i = 5; i >= 1; i--) {
+//     for (j = 0; j < i; j++) {
+//         show += " * ";
+//     }
+//     show += "\n";
+// }
+// console.log(show);
+
+// output of the above code
