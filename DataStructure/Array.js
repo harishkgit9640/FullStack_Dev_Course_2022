7478c1ab86fbb188e681b7c2b4cfcaddb71293a4
Harish Kumar <harishstudio877@gmail.com>
2022-12-04 15:25:51 +0530

    string method in javascript

--- DataStructure/Array.js
+++ DataStructure/Array.js
@@ -16,7 +16,7 @@ console.log(zeroSum(arr));
 
 // find the even number of elements and then make double each number and print the length of the array.
 
-/* 
+/*
 
 let arr = [1, 2, 4, 5, 6, 8, 9, 10, 12, 13];
 let newArr = [];
@@ -30,10 +30,34 @@ for (let i = 0; i < arr.length; i++) {
 console.log(newArr);  //[ 4, 8, 12, 16, 20, 24 ]
 console.log(counter); // 6
 
+*/
 
+// SORTING A ARRAY
+
+/* 
+let arr = [34, 23, 5, 23, 56, 33, 75, 36, 2, 1];
+
+for (i = 0; i < arr.length; i++) {
+    console.log(arr);
+    for (j = 0; j < arr.length; j++) {
+        if (arr[j] > arr[j + 1]) {
+            let temp = arr[j];
+            arr[j] = arr[j + 1];
+            arr[j + 1] = temp;
+        }
+    }
+}
+console.log(arr);
 */
 
 
+// DELETE A ELEMENT WITHIN A ARRAY
 
-
-
+let arr = [45, 32, 41, 23, 64, 21, 5, 73, 4];
+let pos = 1;
+for (i = pos; i < arr.length - 1; i++) {
+    arr[i] = arr[i + 1];
+}
+arr.length = arr.length - 1;
+console.log(arr);
+7
---
+++ DataStructure/String.js
@@ -0,0 +1,22 @@
+// CONVERTING THE STRING TO AN ARRAY
+// let str = 'JAVASCRIPT';
+// console.log([...str]);
+// console.log(str.split());
+// console.log(str.split(","));
+
+
+// REPLACE 
+
+// var str = 'hello how are you?';
+var str = '         hello     ';
+// console.log(str.replace('o', '_'));  // replace first match only
+// console.log(str.replace(/h/g, '#')); // replace all matches letter
+
+// temp = (str.split('o')); //
+// console.log(temp[0]);
+// console.log(str.substring(0, str.length - 1));
+
+// let temp = str.split('').reverse().join('');  // reverse the string
+// console.log(temp);
+
+console.log(str.trim());  // remove the empty space 
